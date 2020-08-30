346c4352f6081bfa87d771911762b47a6ca0da64
Lenny P Critchley <doaltplusf4@gmail.com>
2020-08-30 16:04:38 +0100

    Updated version indicator.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="discordsdk",
-    version="0.2.1",
+    version="0.2.3",
     author="DoAltPlusF4 & NathaanTFM",
     author_email="doaltplusf4@gmail.com",
     description="Python wrapper around Discord's Game SDK library.",
