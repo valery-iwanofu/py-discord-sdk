df350b82aeeff1377c3730843cf309ae2805d15a
Lenny <LennyPhoenixC@gmail.com>
2020-10-06 22:33:42 +0100

    Version update.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="discordsdk",
-    version="0.2.3",
+    version="0.2.4",
     author="LennyPhoenix & NathaanTFM",
     author_email="lennyphoenixc@gmail.com",
     description="Python wrapper around Discord's Game SDK library.",
